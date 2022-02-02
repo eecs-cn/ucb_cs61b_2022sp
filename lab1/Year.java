5765f70e29562a41232640b9f8c482dbebd7e340
EECS CN <eecs.cn@gmail.com>
2022-02-01 17:36:38 -0800

    lab1 done

--- lab1/Year.java
+++ lab1/Year.java
@@ -5,7 +5,7 @@ public class Year {
 
     /** Return true iff YEAR is a leap year.  */
     static boolean isLeapYear(int year) {
-        return true;    // TODO: YOUR CODE HERE
+        return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
     }
 
     /** Print whether YEAR is a a leap year on System.out. */
